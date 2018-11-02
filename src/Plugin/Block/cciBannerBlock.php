028d8c0145a3ed1b9dca5953b2210aecb1bfdc65
Christophe MADEJA <christophe.madeja@niji.fr>
2018-11-02 22:14:08 +0100

    banner

---
+++ src/Plugin/Block/cciBannerBlock.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\cci_banner\Plugin\Block;
+
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'cciBannerBlock' block.
+ *
+ * @Block(
+ *  id = "cci_banner_block",
+ *  admin_label = @Translation("Banner block"),
+ * )
+ */
+class cciBannerBlock extends BlockBase {
+
+  public function build() {
+
+    $img_path = drupal_get_path('module', 'cci_banner') . '/assets/banniere-cci-long.jpg';
+
+    return array(
+      '#theme' => 'cci_banner',
+      '#content' => ['banniere' => $img_path],
+      '#attached' => [
+        'library' => [
+          'cci_banner/cci_banner'
+        ]
+      ]
+    );
+
+  }
+}
